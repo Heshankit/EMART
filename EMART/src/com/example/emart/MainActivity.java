e3d9b5ed2b8042eb0a71ebbfc7b7a7f4dfb09033
heshankit <hikithuldora@gmail.com>
2013-02-02 00:14:08 +0530

    new123

--- EMART/src/com/example/emart/MainActivity.java
+++ EMART/src/com/example/emart/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//why this kolaweri d
+		//hhhhhhhh
 	}
 
 	@Override
